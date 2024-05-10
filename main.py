3c2353bfade876f6a1d1b9f3c304292e8c5ae1d0
Paulina Hudzik <paulinanunab@gmail.com>
2024-05-10 17:05:16 +0200

    połączenie resertu wizualizacji z resetem kół

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.reset_vtk_view_button = self.reset_vtk_view_button
         self.reset_vtk_view_button.clicked.connect(self.resetCamera) # Połączenie przycisku z metodą openVTK
         
-        self.reset_wheel_button= self.reset_wheel_button
-        self.reset_wheel_button.clicked.connect(self.reset_wheel_positions) # Połączenie przycisku z metodą reset_wheel_positions
+        self.reset_visualization_button = self.reset_visualization_button
+        self.reset_visualization_button.clicked.connect(self.reset_vtk_visualization) # Połączenie przycisku z metodą reset_vtk_visualization
 
         self.save_pointcloud_button = self.save_pointcloud_button
         self.save_pointcloud_button.clicked.connect(self.save_pointcloud) # Połączenie przycisku z metodą save_pointcloud
@@ -92,13 +92,28 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.lidarVisualizer.vertices.Reset()
             self.lidarVisualizer.colors.Reset()
 
-            self.reset_wheel_positions() 
+            self.ros_client.wheelL = 0.0
+            self.ros_client.wheelR = 0.0
+            self.ros_client.wheelAvg = 0.0
+            self.ros_client.last_wheelL = 0.0
+            self.ros_client.last_wheelR = 0.0
+
+            # Zresetowanie zmiennych związanych z ruchem kół
+            self.ros_client.vel_angle_z = 0.0
+            self.ros_client.vel_last_angle_z = 0.0
+            self.ros_client.acc_angle_x = 0.0
+            self.ros_client.acc_angle_y = 0.0
+            self.ros_client.acc_angle_z = 0.0
 
 
 
     ########### RESET POZYCJI KÓŁ #############
 
-    def reset_wheel_positions(self):
+    def reset_vtk_visualization(self):
+        self.lidarVisualizer.points.Reset()
+        self.lidarVisualizer.vertices.Reset()
+        self.lidarVisualizer.colors.Reset()
+
         self.ros_client.wheelL = 0.0
         self.ros_client.wheelR = 0.0
         self.ros_client.wheelAvg = 0.0
